26374a5de1eafe1eedc5a53fc48e063743deaf5a
Pierre ROMAN <flaeur@gmail.com>
2024-03-19 20:13:40 +0100

    Exerce 3: basic

--- src/exercise/3-form/03.jsx
+++ src/exercise/3-form/03.jsx
@@ -1,16 +1,14 @@
 const UserForm = ({ onSubmitUser }) => {
-  // 🦁 Crée une fonction handleSubmit qui prend `event` en paramètre.
-  // Récupère ensuite les deux données du formulaire (name et password)
-  // Pour ça tu peux t'aider en loggant dans la console `event.currentTarget` et en cherchant les propriétés name et password.
-  // 💡 Tu peux utiliser `event.currentTarget.elements` pour récupérer les éléments du formulaire.
+  const _handleSubmit = (event) => {
+    const name = event.currentTarget.elements.name.value;
+    const password = event.currentTarget.elements.password.value;
 
-  // 🦁 Appelle la fonction `onSubmitUser` avec les deux données du formulaire dans un object
-  // 💡 onSubmitUser({ name, password })
-  // 💡 Ajoute event.preventDefault() pour éviter que la page ne se recharge
+    onSubmitUser({ name, password });
+    event.preventDefault();
+  };
 
   return (
-    // 🦁 ajoute onSubmit en passant la fonction handleSubmit
-    <form className="vertical-stack form">
+    <form className="vertical-stack form" onSubmit={_handleSubmit}>
       <label>
         Name
         <input type="text" name="name" />
