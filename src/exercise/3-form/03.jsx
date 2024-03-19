48ed420893dbad770502236cce5308ed67eac5bd
Pierre ROMAN <flaeur@gmail.com>
2024-03-19 21:11:19 +0100

    Exercice 3: useForm

--- src/exercise/3-form/03.jsx
+++ src/exercise/3-form/03.jsx
@@ -1,48 +1,35 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const UserForm = ({ onSubmitUser }) => {
-  const [error, setError] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const _handleSubmit = (event) => {
-    if (password.length < 8) {
-      setError('Password must be at least 8 characters long');
-    }
-
-    onSubmitUser({ name, password });
-    event.preventDefault();
-  };
-
-  const _handleChangeName = (event) => {
-    setName(event.target.value);
-  };
-
-  const _handleChangePassword = (event) => {
-    setPassword(event.target.value);
-    setError('');
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
-    <form className="vertical-stack form" onSubmit={_handleSubmit}>
+    <form className="vertical-stack form" onSubmit={handleSubmit(onSubmitUser)}>
       <label htmlFor="name">
         Name
-        <input
-          onChange={_handleChangeName}
-          value={name}
-          type="text"
-          name="name"
-        />
+        <input type="text" name="name" {...register('name')} />
       </label>
       <label htmlFor="password">
         Password
         <input
-          value={password}
           type="password"
           name="password"
-          onChange={_handleChangePassword}
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Password must be at least 8 characters long',
+            },
+          })}
         />
       </label>
-      <div style={{ color: 'red' }}>{error}</div>
+      {errors.password && (
+        <div style={{ color: 'red' }}>{errors.password.message}</div>
+      )}
       <input type="submit" value="Submit" />
     </form>
   );
